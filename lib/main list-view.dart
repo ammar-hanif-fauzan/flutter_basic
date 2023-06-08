a892e11acbb003cf9923adf7785160b027156bcc
haniffrozen <haniffrozen@gmail.com>
2023-06-08 17:01:11 +0700

    list tile

---
+++ lib/main list-view.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+  // ============================ Variable ================================
+  // deklarasi variable 1 <Widget> lebih fleksibel untuk widget lebih dari 1
+  // daripada spesifik <Container>, karena didalamnya harus widget Container
+  // List<Widget> myList = [
+  //   Container(
+  //     width: 300,
+  //     height: 300,
+  //     color: Colors.red,
+  //   ),
+  //   Container(
+  //     width: 300,
+  //     height: 300,
+  //     color: Colors.green,
+  //   ),
+  //   Container(
+  //     width: 300,
+  //     height: 300,
+  //     color: Colors.blue,
+  //   ),
+  //   Container(
+  //     width: 300,
+  //     height: 300,
+  //     color: Colors.amber,
+  //   ),
+  // ];
+
+  List<Color> myColor = [
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.amber,
+  ];
+
+  final List<Widget> myNumber = List.generate(
+    100,
+    (index) => Text(
+      "${index + 1}",
+      style: TextStyle(
+        fontSize: 20 + double.parse(index.toString()),
+      ),
+    ),
+  );
+
+  // ============================= End of Variable ========================
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('List View'),
+        ),
+        // ==================== myList ======================
+        // body: ListView(
+        //   // scrollDirection: Axis.horizontal,
+        //   // tergantung direction width dan heightnya
+        //   children: myList,
+        // )
+        // ==================== End of myList ================
+        // ==================== myColor ======================
+        // body: ListView.builder(
+        //   itemCount: myColor.length,
+        //   itemBuilder: (context, index) {
+        //     return Container(
+        //       height: 300,
+        //       width: 300,
+        //       color: myColor[index],
+        //     );
+        //   },
+        // ),
+        // ====================== End of myColor ==============
+        // ================== myNumber ========================
+        body: ListView(
+          children: myNumber,
+        ),
+        // ================== End of myNumber =================
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,62 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  // ============================ Variable ================================
-  // deklarasi variable 1 <Widget> lebih fleksibel untuk widget lebih dari 1
-  // daripada spesifik <Container>, karena didalamnya harus widget Container
-  // List<Widget> myList = [
-  //   Container(
-  //     width: 300,
-  //     height: 300,
-  //     color: Colors.red,
-  //   ),
-  //   Container(
-  //     width: 300,
-  //     height: 300,
-  //     color: Colors.green,
-  //   ),
-  //   Container(
-  //     width: 300,
-  //     height: 300,
-  //     color: Colors.blue,
-  //   ),
-  //   Container(
-  //     width: 300,
-  //     height: 300,
-  //     color: Colors.amber,
-  //   ),
-  // ];
+  const MyApp({super.key});
 
-  List<Color> myColor = [
-    Colors.red,
-    Colors.green,
-    Colors.blue,
-    Colors.amber,
-  ];
-
-  final List<Widget> myNumber = List.generate(
-    100,
-    (index) => Text(
-      "${index + 1}",
-      style: TextStyle(
-        fontSize: 20 + double.parse(index.toString()),
-      ),
-    ),
-  );
-
-  // ============================= End of Variable ========================
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: Text('List View'),
+          title: const Text('List Tile'),
         ),
-        // ==================== myList ======================
-        // body: ListView(
-        //   // scrollDirection: Axis.horizontal,
-        //   // tergantung direction width dan heightnya
-        //   children: myList,
-        // )
-        // ==================== End of myList ================
-        // ==================== myColor ======================
-        // body: ListView.builder(
-        //   itemCount: myColor.length,
-        //   itemBuilder: (context, index) {
-        //     return Container(
-        //       height: 300,
-        //       width: 300,
-        //       color: myColor[index],
-        //     );
-        //   },
-        // ),
-        // ====================== End of myColor ==============
-        // ================== myNumber ========================
         body: ListView(
-          children: myNumber,
+          children: const [
+            ListTile(
+              contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              title: Text('Daban'),
+              subtitle: Text(
+                'Message... Message...Message...Message...Message...Message...Message...Message...Message...Message...',
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text('10:00 PM'),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text('Daban'),
+              subtitle: Text(
+                'Message...',
+                maxLines: 2,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text('10:00 PM'),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+            ListTile(
+              title: Text('Daban'),
+              subtitle: Text(
+                'Message...',
+                maxLines: 2,
+              ),
+              leading: CircleAvatar(),
+              trailing: Text('10:00 PM'),
+            ),
+            Divider(
+              color: Colors.grey,
+            ),
+          ],
         ),
-        // ================== End of myNumber =================
       ),
     );
   }
