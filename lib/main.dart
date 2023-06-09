7c2a08fe78ca3aed34ff8c07cd347bf91e57dc96
haniffrozen <haniffrozen@gmail.com>
2023-06-09 14:47:35 +0700

    Done Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
+// ignore_for_file: must_be_immutable
+
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +29,37 @@ class MyApp extends StatelessWidget {
         body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            const Text(
-              "1",
-              style: TextStyle(
+            Text(
+              counter.toString(),
+              style: const TextStyle(
                 fontSize: 20,
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: const [
-                // RaisedButton(
-                //   onPressed: () {},
-                //   child: Icon(Icons.remove),
-                // ),
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    if (counter > 1) {
+                      setState(() {
+                        counter--;
+                      });
+                    }
+                    print(counter);
+                  },
+                  child: const Icon(Icons.remove),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (counter >= 1) {
+                      setState(() {
+                        counter++;
+                      });
+                    }
+                    print(counter);
+                  },
+                  child: const Icon(Icons.add),
+                ),
               ],
             )
           ],
